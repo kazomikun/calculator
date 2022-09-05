e2559f2dbce3a10ad9382f0b82a1acac04a0d511
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-09-05 17:12:04 +0100

    added JS to click buttons

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const form = document.getElementById("calc_form")
-
+console.log("hello");
+// console.log(form);
 form.addEventListener("submit", (e) => {
   e.preventDefault()
 })
+
+const output = document.getElementById("output")
+// console.log(output.value);
+// const form = document.getElementById("calc_form")
+const operand_btns = document.querySelectorAll("button[data-type=operand]")
+// console.log(operand_btns);
+let is_operator = false
+operand_btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (output.value == "0") {
+      output.value = e.target.value
+      console.log(e.target.value);
+    } else if (output.value.includes(".")) {
+      output.value = output.value + "" + e.target.value.replace(".","")
+      console.log(e.target.value);
+  } else if (is_operator) {
+      is_operator = false
+      output.value = e.target.value
+    } else {
+      output.value = output.value + "" + e.target.value
+    }
+  })
+});
